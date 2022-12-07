59c3222f7a231ad66baccde711ad5820fbeefbc1
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-08 03:39:51 +0400

    commited for safety

---
+++ PustokApp/Pustok/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+using Pustok.Areas.Admin.ViewModels;
+using Pustok.DAL;
+using Pustok.Helpers;
+using Pustok.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Pustok.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BookController : Controller
+    {
+        private readonly PustokDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public BookController(PustokDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page = 1)
+        {
+
+            var query = _context.Books
+                .Include(x => x.Author)
+                .Include(x => x.BookImages)
+                .Include(x => x.Genre);
+
+            var model = PaginatedList<Book>.Create(query, page, 10);
+
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Genres = _context.Genres.ToList();
+            ViewBag.Authors = _context.Authors.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+
+            CheckImg(book.ImageFiles, book.PosterImg, book.HoverImg);
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Genres = _context.Genres.ToList();
+                ViewBag.Authors = _context.Authors.ToList();
+                return View();
+            }
+
+
+            // saving the images 
+            book.BookImages = new List<BookImage>();
+
+
+
+            BookImage bookPoster = new BookImage
+            {
+                Book = book,
+                PosterStatus = true,
+                Image = FileManager.Save(book.PosterImg, _env.WebRootPath, "Uploads/Books", 100)
+            };
+            book.BookImages.Add(bookPoster);
+            BookImage bookHover = new BookImage
+            {
+                Book = book,
+                PosterStatus = false,
+                Image = FileManager.Save(book.HoverImg, _env.WebRootPath, "Uploads/Books", 100)
+            };
+            book.BookImages.Add(bookHover);
+
+
+            if (book.ImageFiles != null)
+            {
+                foreach (var img in book.ImageFiles)
+                {
+                    BookImage bookImage = new BookImage
+                    {
+                        Book = book,
+                        PosterStatus = null,
+                        Image = FileManager.Save(img, _env.WebRootPath, "Uploads/Books", 100)
+                    };
+                    book.BookImages.Add(bookImage);
+                }
+            }
+
+            book.CreatedAt = DateTime.UtcNow.AddHours(4);
+            book.ModifiedAt = DateTime.UtcNow.AddHours(4);
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+
+
+            var model = _context.Books.Include(x => x.Author).Include(x => x.BookImages).Include(x => x.Genre).FirstOrDefault(x => x.Id == id);
+            ViewBag.Genres = _context.Genres.ToList();
+            ViewBag.Authors = _context.Authors.ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            CheckImgEdit(book.ImageFiles, book.PosterImg, book.HoverImg);
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Genres = _context.Genres.ToList();
+                ViewBag.Authors = _context.Authors.ToList();
+                var model = _context.Books.Include(x => x.Author).Include(x => x.BookImages).Include(x => x.Genre).FirstOrDefault(x => x.Id == book.Id);
+
+                return View(model);
+            }
+            book.BookImages = new List<BookImage>();
+          
+            
+
+            if (book.PosterImg != null)
+            {
+                // remove old one
+                BookImage oldBookImage = _context.BookImages.FirstOrDefault(x => x.PosterStatus == true && x.Book.Id == book.Id);
+                FileManager.Delete(_env.WebRootPath, "Uploads/Books",oldBookImage.Image);
+                _context.BookImages.Remove(oldBookImage);
+
+                //save new one
+                BookImage bookPoster = new BookImage
+                {
+                    Book = book,
+                    PosterStatus = true,
+                    Image = FileManager.Save(book.PosterImg, _env.WebRootPath, "Uploads/Books", 100)
+                };
+                book.BookImages.Add(bookPoster);
+
+            }
+
+            if(book.HoverImg != null)
+            {
+                // remove old one
+                BookImage oldBookImage = _context.BookImages.FirstOrDefault(x => x.PosterStatus == false && x.Book.Id == book.Id);
+                FileManager.Delete(_env.WebRootPath, "Uploads/Books", oldBookImage.Image);
+                _context.BookImages.Remove(oldBookImage);
+
+                // save new one
+                BookImage bookHover = new BookImage
+                {
+                    Book = book,
+                    PosterStatus = false,
+                    Image = FileManager.Save(book.HoverImg, _env.WebRootPath, "Uploads/Books", 100)
+                };
+                book.BookImages.Add(bookHover);
+
+            }
+
+            if (book.ImageFiles != null)
+            {
+                foreach (var img in book.ImageFiles)
+                {
+                    BookImage bookImage = new BookImage
+                    {
+                        Book = book,
+                        PosterStatus = null,
+                        Image = FileManager.Save(img, _env.WebRootPath, "Uploads/Books", 100)
+                    };
+                    book.BookImages.Add(bookImage);
+                }
+            }
+
+
+
+
+
+            List<int> oldBookImages = _context.BookImages.Where(x => x.BookId == book.Id).Select(x => x.Id).ToList();
+
+            List<int> removedBookImgs = oldBookImages.AsQueryable().Except(book.BookImageIds).ToList();
+
+            // delete the removed images
+            foreach (var bkImgId in removedBookImgs)
+            {
+                var imageName = _context.BookImages.FirstOrDefault(x => x.Id == bkImgId).Image;
+                FileManager.Delete(_env.WebRootPath, "Uploads/Books", imageName);
+                _context.BookImages.Remove(_context.BookImages.FirstOrDefault(x => x.Id == bkImgId));
+            }
+
+
+
+
+            return Ok(book);
+        }
+        private void CheckImg(List<IFormFile> imageFiles, IFormFile posterImg, IFormFile hoverImg)
+        {
+            if (posterImg == null)
+                ModelState.AddModelError("PosterImg", "This field is Required");
+            else if (posterImg.ContentType != "image/jpeg" && posterImg.ContentType != "image/png")
+                ModelState.AddModelError("PosterImg", "File Type Must Be JPEG, JPG or PNG");
+            else if (posterImg.Length > 2097152)
+                ModelState.AddModelError("PosterImg", "File size must be less than 2MB!");
+
+
+            if (hoverImg == null)
+                ModelState.AddModelError("HoverImg", "This field is Required");
+            else if (hoverImg.ContentType != "image/jpeg" && hoverImg.ContentType != "image/png")
+                ModelState.AddModelError("HoverImg", "File Type Must Be JPEG, JPG or PNG");
+            else if (hoverImg.Length > 2097152)
+                ModelState.AddModelError("HoverImg", "File size must be less than 2MB!");
+
+            if (imageFiles != null)
+                foreach (var file in imageFiles)
+                {
+                    if (file.ContentType != "image/jpeg" && hoverImg.ContentType != "image/png")
+                        ModelState.AddModelError("HoverImg", "File Type Must Be JPEG, JPG or PNG");
+                    else if (file.Length > 2097152)
+                        ModelState.AddModelError("HoverImg", "File size must be less than 2MB!");
+                }
+        }
+        private void CheckImgEdit(List<IFormFile> imageFiles, IFormFile posterImg, IFormFile hoverImg)
+        {
+
+            if (posterImg != null && posterImg.ContentType != "image/jpeg" && posterImg.ContentType != "image/png")
+                ModelState.AddModelError("PosterImg", "File Type Must Be JPEG, JPG or PNG");
+            if (posterImg != null && posterImg.Length > 2097152)
+                ModelState.AddModelError("PosterImg", "File size must be less than 2MB!");
+
+
+
+            if (hoverImg != null && hoverImg.ContentType != "image/jpeg" && hoverImg.ContentType != "image/png")
+                ModelState.AddModelError("HoverImg", "File Type Must Be JPEG, JPG or PNG");
+            if (hoverImg != null && hoverImg.Length > 2097152)
+                ModelState.AddModelError("HoverImg", "File size must be less than 2MB!");
+
+            if (imageFiles != null)
+                foreach (var file in imageFiles)
+                {
+                    if (file.ContentType != "image/jpeg" && hoverImg.ContentType != "image/png")
+                        ModelState.AddModelError("HoverImg", "File Type Must Be JPEG, JPG or PNG");
+                    else if (file.Length > 2097152)
+                        ModelState.AddModelError("HoverImg", "File size must be less than 2MB!");
+                }
+        }
+
+    }
+}
+
---
+++ PustokApp/Pustok/Models/Book.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pustok.Models
+{
+    public class Book: BaseEntity
+    {
+        public int GenreId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public bool StockStatus { get; set; }
+        public bool IsSpecial { get; set; }
+        public bool IsNew { get; set; }
+
+        [Column(TypeName ="money")]
+        public decimal CostPrice { get; set; }
+
+
+        [Column(TypeName = "money")]
+        public decimal SalePrice { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DiscountPercent { get; set; }
+        [NotMapped]
+        public IFormFile? PosterImg { get; set; }
+        [NotMapped]
+        public IFormFile? HoverImg { get; set; }
+        [NotMapped]
+        public List<IFormFile>? ImageFiles { get; set; }
+
+        [NotMapped]
+        public List<int> BookImageIds { get; set; }
+
+
+
+
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+
+
+        public List<Review> Reviews { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public List<BookImage> BookImages { get; set; }
+    }
+}
